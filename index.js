dfe9ee102d3dcfcac19e2c17d844b8ec27b7c0b6
Takumi Kanada <candy12t@gmail.com>
2021-01-12 15:02:26 +0900

    add: error handling

--- index.js
+++ index.js
@@ -9,17 +9,34 @@ button.textContent = "Push Me";
 document.body.appendChild(button);
 
 const userId = "candy12t";
-fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-	.then(response => {
-		console.log(response.status);
-		return response.json().then(userInfo => {
-			console.log(userInfo);
+const fetchUserInfo = userId => {
+	fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+		.then(response => {
+			console.log(response.status);
+			if(!response.ok) {
+				console.error("error response", response);
+			} else {
+				return response.json().then(userInfo => {
+					console.log(userInfo);
+				});
+			}
+		}).catch(error => {
+			console.error(error);
 		});
-	});
+};
 
-// fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-// 	.then(response => {
-// 		console.log(response.status);
-// 		return response.json();
-// 	})
-// 	.then(userInfo => console.log(userInfo));
+// const fetchUserInfo = userId => {
+// 	fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+// 		.then(response => {
+// 			console.log(response.status);
+// 			if(!response.ok) {
+// 				console.error("error response", response);
+// 			} else {
+// 				return response.json();
+// 			}
+// 		})
+// 		.then(userInfo => console.log(userInfo))
+// 		.catch(error => {
+// 			console.error(error);
+// 		});
+// };
